24fd2413e9ade7ab385d58a8bb50427696c1ba9d
Charles Wang <chaowangbuaa@gmail.com>
2021-09-05 14:44:11 +1200

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Charles Wang on 5/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
